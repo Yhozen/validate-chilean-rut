834dbfce6b644c8c6239c93dbe9b8b5aadc58ebe
Gabriel P??rez <gabriel@garox.org>
2022-11-04 15:43:18 -0300

    add a couple of base functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,40 @@
 export const clearRut = (rut: number | string) => {
   if (typeof rut === "number") return `${rut}`;
 
-  return rut.replace(/\./g, "").replace("-", "");
+  return rut.replace(/\./g, "").replace("-", "").toLowerCase();
+};
+
+export const splitRut = (cleanRut: string) => {
+  const [checkDigit, ...inverseRut] = cleanRut.split("").reverse();
+
+  return { checkDigit, inverseRut };
+};
+
+const DIGITS_TO_SUM = [2, 3, 4, 5, 6, 7];
+
+export const getRutSum = (stringDigitArray: string[]) => {
+  return stringDigitArray.reduce((accumulator, digit, index) => {
+    const multiplication =
+      DIGITS_TO_SUM[index % DIGITS_TO_SUM.length] * Number(digit);
+
+    return accumulator + multiplication;
+  }, 0);
+};
+
+export const getCheckDigitFromSum = (sum: number): string => {
+  const remainder = 11 - (sum % 11);
+  if (remainder === 10) return "k";
+
+  return String(remainder);
+};
+
+export const getCheckDigit = (
+  rutWithoutCheckDigit: number | string
+): string => {
+  const cleanRut = clearRut(rutWithoutCheckDigit);
+  const inverseRut = cleanRut.split("").reverse();
+
+  const sum = getRutSum(inverseRut);
+
+  return getCheckDigitFromSum(sum);
 };
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,11 @@
 import { describe, it, expect } from "vitest";
-import { clearRut } from "../src";
+import {
+  clearRut,
+  getCheckDigit,
+  getCheckDigitFromSum,
+  getRutSum,
+  splitRut,
+} from "../src";
 
 describe("clearRut", () => {
   it("receive a number rut", () => {
@@ -12,5 +18,40 @@ describe("clearRut", () => {
 
   it("receive with dots and dash", () => {
     expect(clearRut("96.591.760-8")).toBe("965917608");
+  });
+});
+
+describe("splitRut", () => {
+  it("separate checkDigit and get inverseRut array", () => {
+    const { checkDigit, inverseRut } = splitRut("965917608");
+
+    expect(checkDigit).toBe("8");
+    expect(inverseRut.join("")).toBe("06719569");
+  });
+});
+
+describe("getRutSum", () => {
+  it("get an inverseRut digit array and return the desired value", () => {
+    expect(getRutSum("97587621".split(""))).toBe(190);
+  });
+});
+
+describe("getCheckDigitFromSum", () => {
+  it("get checkDigit as string using sum as input", () => {
+    expect(getCheckDigitFromSum(190)).toBe("8");
+  });
+
+  it("get checkDigit k", () => {
+    expect(getCheckDigitFromSum(232)).toBe("k");
+  });
+});
+
+describe("getCheckDigit", () => {
+  it("get checkDigit from a number", () => {
+    expect(getCheckDigit(76997492)).toBe("k");
+  });
+
+  it("get checkDigit from a string", () => {
+    expect(getCheckDigit("76997492")).toBe("k");
   });
 });
